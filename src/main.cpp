1343a32da013807241ac4cd6d70be58cc54bdbec
April White <april@opsnlops.io>
2021-12-28 23:08:09 -0800

    Clean up the spacing

--- src/main.cpp
+++ src/main.cpp
@@ -13,52 +13,52 @@
 void setup()
 {
 
-  // Open serial communications and wait for port to open:
-  Serial.begin(19200);
-  while (!Serial)
-    ;
+    // Open serial communications and wait for port to open:
+    Serial.begin(19200);
+    while (!Serial)
+        ;
 
-  // Nice long delay to let the terminal app start
-  delay(2500);
-  log_i("Helllllo! I'm up and running on on %s!", ARDUINO_VARIANT);
+    // Nice long delay to let the terminal app start
+    delay(2500);
+    log_i("Helllllo! I'm up and running on on %s!", ARDUINO_VARIANT);
 
-  WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
+    WiFi.begin(WIFI_NETWORK, WIFI_PASSWORD);
 
-  /*wait until ESP32 connect to WiFi*/
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(500);
-    log_d(" (not yet)");
-  }
-  log_i("WiFi connected to %s", WIFI_NETWORK);
-
-  if (!MDNS.begin("esp32-mdns"))
-  {
-    log_e("Error setting up MDNS responder!");
-    while (1)
+    /*wait until ESP32 connect to WiFi*/
+    while (WiFi.status() != WL_CONNECTED)
     {
-      delay(1000);
+        delay(500);
+        log_d(" (not yet)");
     }
-  }
-  log_i("MDNS set up");
+    log_i("WiFi connected to %s", WIFI_NETWORK);
 
-  // Register a fake server
-  MDNS.addService("creatures", "tcp", 666);
-  MDNS.addServiceTxt("creatures", "tcp", "board", ARDUINO_VARIANT);
-  log_d("created a fake service");
+    if (!MDNS.begin("esp32-mdns"))
+    {
+        log_e("Error setting up MDNS responder!");
+        while (1)
+        {
+            delay(1000);
+        }
+    }
+    log_i("MDNS set up");
 
-  MagicBroker broker = MagicBroker();
-  if (broker.find())
-  {
-    log_i("found the magic broker! %s", broker.ipAddress.toString().c_str());
-  }
-  else
-  {
-    log_e("unable to find the magic broker (role is suppose to be '%s')", broker.getMagicRole());
-  }
+    // Register a fake server
+    MDNS.addService("creatures", "tcp", 666);
+    MDNS.addServiceTxt("creatures", "tcp", "board", ARDUINO_VARIANT);
+    log_d("created a fake service");
+
+    MagicBroker broker = MagicBroker();
+    if (broker.find())
+    {
+        log_i("found the magic broker! %s", broker.ipAddress.toString().c_str());
+    }
+    else
+    {
+        log_e("unable to find the magic broker (role is suppose to be '%s')", broker.getMagicRole());
+    }
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+    // put your main code here, to run repeatedly:
 }
